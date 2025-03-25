bb48619a599ccc0126ceba489b58ccb6bfe2aef0
Jean Mardochée JOSEPH <josephjeanmardochee@gmail.com>
2025-03-25 23:30:21 +0100

    Update project

--- setup_project.sh
+++ setup_project.sh
@@ -1,28 +1,46 @@
 #!/bin/bash
 
 WORKSPACE_DIR="/home/vagrant/workspace"
+WORKSPACE_PROJECT="/home/vagrant/workspace/project-devops-v1"
+SSH_KEY_PATH="/home/vagrant/.ssh/id_rsa"
+WORKSPACE_DOCKER="/home/vagrant/workspace/project-devops-v1/simple_api"
+source .env
 
-# Démarrer l'agent SSH et charger la clé
-eval "$(ssh-agent -s)"
-ssh-add /home/vagrant/.ssh/id_rsa
+echo "Starting SSH agent..."
+eval "$(ssh-agent -s)" || { echo "Failed to start ssh-agent"; exit 1; }
+ssh-add "$SSH_KEY_PATH"
 sleep 2
 
-# Créer le dossier workspace
-echo "create folder workspace and change dir"
+echo "Creating workspace folder..."
 mkdir -p "$WORKSPACE_DIR"
 chown vagrant:vagrant "$WORKSPACE_DIR"
 chmod 700 "$WORKSPACE_DIR"
 cd "$WORKSPACE_DIR"
 
-# Cloner le dépôt
-if [ ! -d "$WORKSPACE_DIR/project-devops-v1" ]; then
-    echo "git clone project in /home/vagrant/workspace"
-  # sudo -u vagrant -H bash -c 'GIT_SSH_COMMAND="ssh -i /home/vagrant/.ssh/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git clone git@github.com:Jean1084/project-devops-v1.git'
-    sudo -u vagrant -H bash -c 'GIT_SSH_COMMAND="ssh -i /home/vagrant/.ssh/id_rsa -o StrictHostKeyChecking=accept-new" git clone git@github.com:Jean1084/project-devops-v1.git'
-    chown -R vagrant:vagrant "$WORKSPACE_DIR/project-devops-v1"
-    chmod -R 700 "$WORKSPACE_DIR/project-devops-v1"
-    echo "git clone done!"
+if [ ! -d "$WORKSPACE_PROJECT/.git" ]; then
+    echo "Cloning project into '$WORKSPACE_PROJECT'..."
+    sudo -u vagrant -H bash -c "GIT_SSH_COMMAND='ssh -i $SSH_KEY_PATH -o StrictHostKeyChecking=accept-new' git clone git@github.com:Jean1084/project-devops-v1.git $WORKSPACE_PROJECT"
+    chown -R vagrant:vagrant "$WORKSPACE_PROJECT"
+    chmod -R 700 "$WORKSPACE_PROJECT"
+    echo "Git clone done!"
 else
-    echo "git clone failed! Directory already exists."
-    exit 1
+    echo "Project already exists, pulling latest changes..."
+    cd "$WORKSPACE_PROJECT"
+    sudo -u vagrant -H git pull origin main
 fi
+
+# Change directory Dockerfile
+# cd "$WORKSPACE_DOCKER"
+
+# Create image
+# docker build -t simple-api-jean .
+
+# Connexion a Docker
+# echo "Connexion à Docker avec l'utilisateur : $DOCKER_USER"
+# echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin 
+
+# Preparation de l'image
+# docker tag simple-api-jean jean1084/simple-api-jean:latest
+
+# Envoi de l'image sur Docker hub
+# docker push jean1084/simple-api-jean:latest
